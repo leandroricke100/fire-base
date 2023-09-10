763aa87272cdf4df51f3e6c721a2cadd3439ff20
leandroricke100 <leandro_henrique_100@hotmail.com>
2023-09-10 14:26:53 -0300

    first commit

---
+++ src/firebaseConnection.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAsq76Jm6v0Zw6OvVjVu2W810_e1fwmNIM",
+  authDomain: "curso-a73c0.firebaseapp.com",
+  projectId: "curso-a73c0",
+  storageBucket: "curso-a73c0.appspot.com",
+  messagingSenderId: "324031824851",
+  appId: "1:324031824851:web:c379f5aa8d61fdbcdc2526",
+  measurementId: "G-6CBMNNNKCG",
+};
