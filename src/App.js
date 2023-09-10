aa7177ef6556e9d5f76762564854b66cb027adfd
leandroricke100 <leandro_henrique_100@hotmail.com>
2023-09-10 14:41:04 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { db } from "./firebaseConnection";
+import "./app.css";
+
 function App() {
+  function handleAdd() {
+    alert("teste");
+  }
+
   return (
     <div className="App">
       <h1>REACT + FIREBASE</h1>
+
+      <div className="container">
+        <label>Título:</label>
+        <textarea typeof="text" placeholder="Digite um titulo" />
+
+        <label>Autor:</label>
+        <input typeof="text" placeholder="Autor do post" />
+
+        <button onClick={handleAdd}>Cadastrar</button>
+      </div>
     </div>
   );
 }
--- src/firebaseConnection.js
+++ src/firebaseConnection.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAsq76Jm6v0Zw6OvVjVu2W810_e1fwmNIM",
   authDomain: "curso-a73c0.firebaseapp.com",
@@ -7,3 +10,9 @@ const firebaseConfig = {
   appId: "1:324031824851:web:c379f5aa8d61fdbcdc2526",
   measurementId: "G-6CBMNNNKCG",
 };
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+
+export { db };
